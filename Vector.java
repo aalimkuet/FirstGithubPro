0a7c5aebbc86ba9ed9472a05e5886f780e4ddec5
aalimkuet <aalimkuet@gmail.com>
2016-12-18 16:07:06 +0600

    gfhjghihuihuobuiobjkl

--- Vector.java
+++ Vector.java
@@ -1,5 +1,36 @@
 package labtest;
 
-public class Vector {
+public class Vector implements VectorInterface{
+
+	public int x , y;
+  	
+	  public Vector(int x, int y) {
+        this.x=x;
+        this.y=y;
+		
+	}
+	  
+	  
+	public int len() {
+		return x;
+		
+	}
+
+	
+	public double Magnitude() {
+		
+		return  Math.sqrt(x*x+y*y) ;
+	}
+
+	public double Theta() {
+		
+		return Math.atan(y/x);
+	}
+
+	public void ShowVector() {
+		
+		System.out.println("x = "+ x + "y = " + y);
+	}
+	
 
 }
